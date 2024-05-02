165cda04dac1c984fd91b9413e2457c8df39e2c8
beyerperez123@gmail.com <beyerperez123@gmail.com>
2024-05-02 12:52:09 -0600

    ⭐Fin slost

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,22 @@ import FanyButton from './components/FanyButton.vue';
 
 <template>
 
-  <div class="content">
-    <FanyButton/>
-  </div>
+  <FanyButton>
+    <template v-slot:icon="slotProps">
+      {{ slotProps.hover ? '🚀' : '🍂' }}
+    </template>
+    <template v-slot:default>
+      <strong style="text-decoration: underline;">E</strong>nviar
+    </template>
+  </FanyButton>
+  <FanyButton>
+    <template #icon="slotProps">
+      {{ slotProps.hover ? '🪅' : '🐷' }}
+    </template>
+    <template #default>
+      <strong style="text-decoration: underline;">C</strong>ancelar
+    </template>
+  </FanyButton>
 </template>
 
 <style scoped>
--- src/components/FanyButton.vue
+++ src/components/FanyButton.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { ref } from 'vue'
+const hover = ref(false);
 </script>
 
 <template>
-
   <div class="content">
-    <button> Haz Clic 🐶</button>
+    <button 
+    @mouseenter="hover = true"
+    @mouseleave="hover = false"> 
+      <slot name="icon" :hover="hover">⚡</slot>
+      <slot> No Message </slot> 
+    </button>
+  
   </div>
 </template>
 
